24f4d35a7f2e328e7bb08353e8ba6ece46fa9de9
beyourahi <beyourahi@gmail.com>
2026-08-17 22:44:09 +0600

    build: sync Dropout DS 2.0.0

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -197,7 +197,10 @@
         icon={Cloud}
     >
         {#snippet header()}
-            <StatusBadge {connected} />
+            <StatusBadge
+                label={connected ? "Connected" : "Not connected"}
+                tone={connected ? "connected" : "disconnected"}
+            />
         {/snippet}
 
         <form
